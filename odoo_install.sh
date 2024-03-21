c7df97e3a573ac33c0a6a350cb37766560d13b81
jescobarv-git <jescobarv@gmail.com>
2024-03-21 12:41:51 -0500

    modify to vmc standard

--- odoo_install.sh
+++ odoo_install.sh
@@ -15,8 +15,9 @@
 ################################################################################
 
 OE_USER="odoo"
-OE_HOME="/$OE_USER"
-OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
+OE_HOME="/opt/$OE_USER"
+OE_CONFIG="${OE_USER}-server"
+OE_HOME_EXT="$OE_HOME/$OE_CONFIG"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
@@ -35,7 +36,6 @@ INSTALL_NGINX="False"
 OE_SUPERADMIN="admin"
 # Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
 GENERATE_RANDOM_PASSWORD="True"
-OE_CONFIG="${OE_USER}-server"
 # Set the website name
 WEBSITE_NAME="_"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
@@ -94,20 +94,6 @@ echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
 #--------------------------------------------------
-# Install Dependencies
-#--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install python3 python3-pip
-sudo apt-get install git python3-cffi build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng-dev libjpeg-dev gdebi -y
-
-echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
-
-echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
-sudo apt-get install nodejs npm -y
-sudo npm install -g rtlcss
-
-#--------------------------------------------------
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
 if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
@@ -156,7 +142,7 @@ if [ $IS_ENTERPRISE = "True" ]; then
     echo -e "\n--- Create symlink for node"
     sudo ln -s /usr/bin/nodejs /usr/bin/node
     sudo su $OE_USER -c "mkdir $OE_HOME/enterprise"
-    sudo su $OE_USER -c "mkdir $OE_HOME/enterprise/addons"
+    sudo su $OE_USER -c "mkdir $OE_HOME/enterprise/custom-addons"
 
     GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/enterprise "$OE_HOME/enterprise/addons" 2>&1)
     while [[ $GITHUB_RESPONSE == *"Authentication"* ]]; do
@@ -177,8 +163,8 @@ if [ $IS_ENTERPRISE = "True" ]; then
 fi
 
 echo -e "\n---- Create custom module directory ----"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
+#sudo su $OE_USER -c "mkdir $OE_HOME/addons"
+sudo su $OE_USER -c "mkdir $OE_HOME/${OE_USER}-custom-addons"
 
 echo -e "\n---- Setting permissions on home folder ----"
 sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
@@ -202,9 +188,9 @@ fi
 sudo su root -c "printf 'logfile = /var/log/${OE_USER}/${OE_CONFIG}.log\n' >> /etc/${OE_CONFIG}.conf"
 
 if [ $IS_ENTERPRISE = "True" ]; then
-    sudo su root -c "printf 'addons_path=${OE_HOME}/enterprise/addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
+    sudo su root -c "printf 'addons_path=${OE_HOME}/enterprise/custom-addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
 else
-    sudo su root -c "printf 'addons_path=${OE_HOME_EXT}/addons,${OE_HOME}/custom/addons\n' >> /etc/${OE_CONFIG}.conf"
+    sudo su root -c "printf 'addons_path=${OE_HOME}/${OE_USER}-custom-addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
 fi
 sudo chown $OE_USER:$OE_USER /etc/${OE_CONFIG}.conf
 sudo chmod 640 /etc/${OE_CONFIG}.conf
@@ -215,83 +201,131 @@ sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/odoo-bin --config=/etc/${OE
 sudo chmod 755 $OE_HOME_EXT/start.sh
 
 #--------------------------------------------------
+# Install Dependencies
+#--------------------------------------------------
+echo -e "\n--- Installing Python 3 + pip3 --"
+sudo su $OE_USER
+cd $OE_HOME
+#reCreate a new Python virtual environment for Odoo
+python3.8 -m venv ${OE_USER}-venv
+#Activate the virtual environment
+source ${OE_USER}-venv/bin/activate
+pip3 install wheel
+echo -e "\n---- Install python packages/requirements ----"
+pip3 install -r $OE_CONFIG/requirements.txt
+#sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+#Deactivate Virtual environment
+deactivate
+exit
+
+echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
+sudo apt-get install nodejs npm -y
+sudo npm install -g rtlcss
+
+
+#--------------------------------------------------
 # Adding ODOO as a deamon (initscript)
 #--------------------------------------------------
 
 echo -e "* Create init file"
-cat <<EOF > ~/$OE_CONFIG
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides: $OE_CONFIG
-# Required-Start: \$remote_fs \$syslog
-# Required-Stop: \$remote_fs \$syslog
-# Should-Start: \$network
-# Should-Stop: \$network
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Enterprise Business Applications
-# Description: ODOO Business Applications
-### END INIT INFO
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
-DAEMON=$OE_HOME_EXT/odoo-bin
-NAME=$OE_CONFIG
-DESC=$OE_CONFIG
-# Specify the user name (Default: odoo).
-USER=$OE_USER
-# Specify an alternate config file (Default: /etc/openerp-server.conf).
-CONFIGFILE="/etc/${OE_CONFIG}.conf"
-# pidfile
-PIDFILE=/var/run/\${NAME}.pid
-# Additional options that are passed to the Daemon.
-DAEMON_OPTS="-c \$CONFIGFILE"
-[ -x \$DAEMON ] || exit 0
-[ -f \$CONFIGFILE ] || exit 0
-checkpid() {
-[ -f \$PIDFILE ] || return 1
-pid=\`cat \$PIDFILE\`
-[ -d /proc/\$pid ] && return 0
-return 1
-}
-case "\${1}" in
-start)
-echo -n "Starting \${DESC}: "
-start-stop-daemon --start --quiet --pidfile \$PIDFILE \
---chuid \$USER --background --make-pidfile \
---exec \$DAEMON -- \$DAEMON_OPTS
-echo "\${NAME}."
-;;
-stop)
-echo -n "Stopping \${DESC}: "
-start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
---oknodo
-echo "\${NAME}."
-;;
-restart|force-reload)
-echo -n "Restarting \${DESC}: "
-start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
---oknodo
-sleep 1
-start-stop-daemon --start --quiet --pidfile \$PIDFILE \
---chuid \$USER --background --make-pidfile \
---exec \$DAEMON -- \$DAEMON_OPTS
-echo "\${NAME}."
-;;
-*)
-N=/etc/init.d/\$NAME
-echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
-exit 1
-;;
-esac
-exit 0
+cat <<EOF > /etc/systemd/system/$OE_USER.service
+[Unit]
+Description=$OE_USER
+Requires=postgresql.service
+After=network.target postgresql.service
+
+[Service]
+Type=simple
+SyslogIdentifier=$OE_USER
+PermissionsStartOnly=true
+User=$OE_USER
+Group=$OE_USER
+ExecStart=$OE_HOME/$OE_USER-venv/bin/python3 $OE_HOME/$OE_USER/odoo-bin -c /etc/$OE_CONFIG.conf
+StandardOutput=journal+console
+
+[Install]
+WantedBy=multi-user.target
 EOF
+#Notify systemd that a new unit file exists
+sudo systemctl daemon-reload
+#Start the Odoo service and enable it to start on boot by running:
+sudo systemctl enable --now $OE_USER
 
-echo -e "* Security Init File"
-sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-sudo chmod 755 /etc/init.d/$OE_CONFIG
-sudo chown root: /etc/init.d/$OE_CONFIG
 
-echo -e "* Start ODOO on Startup"
-sudo update-rc.d $OE_CONFIG defaults
+
+#cat <<EOF > ~/$OE_CONFIG
+##!/bin/sh
+#### BEGIN INIT INFO
+## Provides: $OE_CONFIG
+## Required-Start: \$remote_fs \$syslog
+## Required-Stop: \$remote_fs \$syslog
+## Should-Start: \$network
+## Should-Stop: \$network
+## Default-Start: 2 3 4 5
+## Default-Stop: 0 1 6
+## Short-Description: Enterprise Business Applications
+## Description: ODOO Business Applications
+#### END INIT INFO
+#PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+#DAEMON=$OE_HOME_EXT/odoo-bin
+#NAME=$OE_CONFIG
+#DESC=$OE_CONFIG
+## Specify the user name (Default: odoo).
+#USER=$OE_USER
+## Specify an alternate config file (Default: /etc/openerp-server.conf).
+#CONFIGFILE="/etc/${OE_CONFIG}.conf"
+## pidfile
+#PIDFILE=/var/run/\${NAME}.pid
+## Additional options that are passed to the Daemon.
+#DAEMON_OPTS="-c \$CONFIGFILE"
+#[ -x \$DAEMON ] || exit 0
+#[ -f \$CONFIGFILE ] || exit 0
+#checkpid() {
+#[ -f \$PIDFILE ] || return 1
+#pid=\`cat \$PIDFILE\`
+#[ -d /proc/\$pid ] && return 0
+#return 1
+#}
+#case "\${1}" in
+#start)
+#echo -n "Starting \${DESC}: "
+#start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+#--chuid \$USER --background --make-pidfile \
+#--exec \$DAEMON -- \$DAEMON_OPTS
+#echo "\${NAME}."
+#;;
+#stop)
+#echo -n "Stopping \${DESC}: "
+#start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+#--oknodo
+#echo "\${NAME}."
+#;;
+#restart|force-reload)
+#echo -n "Restarting \${DESC}: "
+#start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+#--oknodo
+#sleep 1
+#start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+#--chuid \$USER --background --make-pidfile \
+#--exec \$DAEMON -- \$DAEMON_OPTS
+#echo "\${NAME}."
+#;;
+#*)
+#N=/etc/init.d/\$NAME
+#echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
+#exit 1
+#;;
+#esac
+#exit 0
+#EOF
+#
+#echo -e "* Security Init File"
+#sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
+#sudo chmod 755 /etc/init.d/$OE_CONFIG
+#sudo chown root: /etc/init.d/$OE_CONFIG
+#
+#echo -e "* Start ODOO on Startup"
+#sudo update-rc.d $OE_CONFIG defaults
 
 #--------------------------------------------------
 # Install Nginx if needed
