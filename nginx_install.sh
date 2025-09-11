9a17ab377d3ae4d3043348a7f22def8c0cf15bb3
Jaime Escobar <61430982+jescobarv-git@users.noreply.github.com>
2025-09-11 15:51:42 -0500

    Update nginx_install.sh client max body size

--- nginx_install.sh
+++ nginx_install.sh
@@ -118,7 +118,7 @@ server {
     proxy_read_timeout 900s;
     proxy_connect_timeout 900s;
     proxy_send_timeout 900s;
-    client_max_body_size 0;
+    client_max_body_size 200M;
 
     gzip on;
     gzip_min_length 1100;
